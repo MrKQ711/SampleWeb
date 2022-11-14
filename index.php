da9b1ad4bae1fe3da06ac026db6b1b2b8e1fb62c
Thái <92800620+MrKQ711@users.noreply.github.com>
2022-11-14 13:05:17 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<!-- connect file -->
-<?php
-include('includes/connect.php');
-include('functions/common_function.php');
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,144 +5,117 @@ include('functions/common_function.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ecommerce Website using PHP and MySQL</title>
-    <!-- // bootstrap CSS link-->
+    <title>Admin Dashboard</title>
+    <!-- bootstrap css link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!-- // font awesome link-->
+    <!-- font awesome link-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- // CSS link -->
-    <link rel="stylesheet" href="style.css">
+    <!-- CSS link -->
+    <link rel="stylesheet" href="../style.css">
+    <style>
+        .admin_image {
+            width: 100px;
+            object-fit: contain;
+        }
+
+        .footer {
+            position: absolute;
+            bottom: 0;
+        }
+    </style>
+
+
 </head>
 
 <body>
+
     <!-- navbar -->
     <div class="container-fluid p-0">
-        <!-- first child-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+        <!-- first child -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-secondary ">
             <div class="container-fluid">
-                <img src="./image/logo.png" alt="" class="logo">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <img src="../image/logo.png" alt="" class="logo">
+                <nav class="navbar navbar-expand-lg">
+                    <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="display_all.php">Products</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Register</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="cart.php"> <i class="fa-sharp fa-solid fa-cart-shopping"></i><sup>
-                                    <?php
-                                    cart_item();
-                                    ?>
-                                </sup></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Total price:
-                                <?php 
-                                totalPrice(); 
-                                ?>
-                            </a>
+                            <a href="" class="nav-link">Welcome</a>
                         </li>
                     </ul>
-                    <form class="d-flex" action="searchProduct.php" method="GET">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_data">
-                        <input type="submit" value="Search" class="btn btn-outline-light" name="search_data_product">
-                    </form>
-                </div>
+                </nav>
             </div>
         </nav>
 
-        <!-- calling cart function -->
-        <?php
-        cart();
-        ?>
-        <!-- second child -->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <ul class="navbar-nav me-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Welcome</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
-                </li>
-            </ul>
-        </nav>
+        <!-- second child-->
+        <div class="bg-gradient">
+            <h3 class=" text-black text-center p-2">Manager Details</h3>
+        </div>
 
         <!-- third child -->
-        <div class="bg-light">
-            <h3 class="text-center">
-                Hidden Store
-            </h3>
-            <p class="text-center">Communication </p>
+        <div class="row">
+            <div class="col-md-12 bg-secondary p-1 d-flex
+            align-items-center">
+                <div class="px-5">
+                    <a href="#"><img src="../image/pineappJuice.jpg" alt="" class="admin_image"></a>
+                    <p class="text-black text-center">Admin</p>
+                </div>
+                <!-- button*10>a.nav-link.text-light.bg-info.my-1 -->
+                <div class="button text-center">
+                    <button class="my-3"><a href="insert_product.php" class="nav-link 
+                    text-light bg-info my-1">Insert Products</a></button>
+
+                    <button><a href="" class="nav-link text-light 
+                    bg-info my-1">View Products</a></button>
+
+                    <button><a href="index.php?insert_category" class="nav-link text-light 
+                    bg-info my-1">Insert Categories</a></button>
+
+                    <button><a href="" class="nav-link text-light 
+                    bg-info my-1">View Categories</a></button>
+
+                    <button><a href="index.php?insert_brand" class="nav-link text-light 
+                    bg-info my-1">Insert Brands</a></button>
+
+                    <button><a href="" class="nav-link text-light 
+                    bg-info my-1">View Brands</a></button>
+
+                    <button><a href="" class="nav-link text-light 
+                    bg-info my-1">All Orders</a></button>
+
+                    <button><a href="" class="nav-link text-light 
+                    bg-info my-1">All Payments</a></button>
+
+                    <button><a href="" class="nav-link text-light 
+                    bg-info my-1">List Users</a></button>
+
+                    <button><a href="" class="nav-link text-light 
+                    bg-info my-1">Log out</a></button>
+                </div>
+            </div>
         </div>
 
         <!-- fourth child -->
-        <div class="row px-1">
-            <div class="col-md-10">
-                <!-- product -->
-                <div class="row px-3">
-                    <!-- fetching group products -->
-                    <?php
-                    getProducts();
-                    getUniqCategories();
-                    getUniqBrand();
-                    // $ip = getIPAddress();
-                    // echo 'User Real IP Address - ' . $ip;
-                    ?>
-                    <!-- row end -->
-                </div>
-                <!-- #col end -->
-            </div>
-            <div class="col-md-2 bg-secondary p-0">
-                <!-- brands to be displayed -->
-                <ul class="navbar-nav me-auto text-center">
-                    <li class="nav-item bg-info">
-                        <a href="#" class="nav-link text-light">
-                            <h4>Delivery Brands</h4>
-                        </a>
-                    </li>
-
-                    <?php
-                    getBrands();
-                    ?>
-
-                </ul>
-
-                <!-- categories to be displayed-->
-                <ul class="navbar-nav me-auto text-center">
-                    <li class="nav-item bg-info">
-                        <a href="#" class="nav-link text-light">
-                            <h4>Categories</h4>
-                        </a>
-                    </li>
-                    <?php
-                    getCategories();
-                    ?>
-                </ul>
-            </div>
+        <!-- If something is exist, then it will display insert_categories.php -->
+        <div class="container my-3">
+            <?php
+            if (isset($_GET['insert_category'])) {
+                include("insert_categories.php");
+            }
+            if (isset($_GET['insert_brand'])) {
+                include("insert_brands.php");
+            }
+            ?>
         </div>
 
         <!-- last child -->
-        <!-- include footer -->
-        <?php
-        include('./includes/footer.php');
-        ?>
+        <div class="bg-secondary p-3 text-center footer">
+            <p>Designed by ThaiNguyen</p>
 
+        </div>
     </div>
 
     <!-- bootstrap js link -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
-
 </body>
 
 </html>
